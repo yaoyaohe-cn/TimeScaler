df00418003e0ba0f0428d6f412c31f23239d72fc
yaoyaohe-cn <108442516+yaoyaohe-cn@users.noreply.github.com>
2026-01-06 16:47:40 +0800

    Create ETTh2.sh

---
+++ scripts/optuna/ETTh2.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+export CUDA_VISIBLE_DEVICES=0
+
+if [ ! -d "./logs/Optuna" ]; then
+    mkdir -p ./logs/Optuna
+fi
+
+
+model_name=TimeScaler
+data=ETTh2
+n_jobs=1
+trial_num=50
+lrs="0.0001 0.005"
+
+pred_lens=(96 192 336 720)
+
+for pred_len in "${pred_lens[@]}"; do
+    echo ">>> Tuning $data | Pred: $pred_len"
+    
+    python -u run_LTF.py \
+       --model $model_name \
+       --data $data \
+       --pred_len $pred_len \
+       --use_hyperParam_optim \
+       --n_jobs $n_jobs \
+       --optuna_trial_num $trial_num \
+       --optuna_lr $lrs \
+       --train_epochs 30 \
+       --patience 5 \
+      > logs/Optuna/${data}_${pred_len}.log 2>&1
+done
